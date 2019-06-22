302bf3c47cccf2091d90fd9e21316869330db939
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2019-06-22 12:29:48 +0200

    Release 6.

---
+++ precache-manifest.223ccf2ca858cc33ca185324945c021d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc57e9bc213171035d1781456402465e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010450bc994039a78688",
+    "url": "/static/css/2.aadf332b.chunk.css"
+  },
+  {
+    "revision": "85d2251e2e3b1c46cb6e",
+    "url": "/static/css/main.bf8c1021.chunk.css"
+  },
+  {
+    "revision": "010450bc994039a78688",
+    "url": "/static/js/2.9b0acc14.chunk.js"
+  },
+  {
+    "revision": "85d2251e2e3b1c46cb6e",
+    "url": "/static/js/main.fffaf8e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b808728cfbf62e13c7aba10b83d9c407.js"
+  "/precache-manifest.223ccf2ca858cc33ca185324945c021d.js"
 );
 
 self.addEventListener('message', (event) => {
