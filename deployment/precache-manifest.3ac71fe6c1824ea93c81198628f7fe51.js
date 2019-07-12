1864f0b223e8ae634e9700aeffee7ae669e8d54e
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2019-07-12 13:42:37 +0200

    Release 0.

---
+++ deployment/precache-manifest.3ac71fe6c1824ea93c81198628f7fe51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f396dcc8772652c68af7a90453a9ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5f3febef94f99a5f35",
+    "url": "/static/css/2.ebfed7b9.chunk.css"
+  },
+  {
+    "revision": "d43435938374283e45f4",
+    "url": "/static/css/main.e2763c37.chunk.css"
+  },
+  {
+    "revision": "3e5f3febef94f99a5f35",
+    "url": "/static/js/2.cc615272.chunk.js"
+  },
+  {
+    "revision": "d43435938374283e45f4",
+    "url": "/static/js/main.843aa861.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
