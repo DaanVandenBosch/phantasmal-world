9cf951003a4f2bee4d78a5279ace3e448b896b6f
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2019-12-26 14:40:12 +0100

    Improved deployment script.

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,12 @@
 # Requires a git ./deployment directory which tracks a gh-pages branch.
 Param([Parameter(Mandatory = $true)][Int32]$release)
 
+Write-Output "Running tests."
+yarn test
+
+Write-Output "Generating production build."
+yarn build
+
 Write-Output "Deleting ./deployment contents."
 Remove-Item -Recurse ./deployment/*
 
@@ -15,3 +21,5 @@ git pull
 git add .
 git commit -m "Release $release."
 git push
+
+Set-Location ..
