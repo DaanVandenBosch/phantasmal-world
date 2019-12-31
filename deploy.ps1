24161fbfa6c3728a7f0548d0bad3fc3d3ac01d05
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2019-12-31 21:18:26 +0100

    Improved deploy script.

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,5 @@
 # Script for deploying to gh-pages.
 # Requires a git ./deployment directory which tracks a gh-pages branch.
-Param([Parameter(Mandatory = $true)][Int32]$release)
 
 Write-Output "Running tests."
 yarn test
@@ -15,11 +14,16 @@ Write-Output "Copying dist to ./deployment."
 Copy-Item -Recurse ./dist/* ./deployment
 Write-Output "www.phantasmal.world" > deployment/CNAME
 
+Write-Output "Bumping version."
+$version = Get-Content -Path version.txt
+$version = $version / 1 + 1
+Write-Output $version > version.txt
+
 Write-Output "Committing and pushing to gh-pages."
 Set-Location ./deployment
 git pull
 git add .
-git commit -m "Release $release."
+git commit -m "Release $version."
 git push
 
 Set-Location ..
