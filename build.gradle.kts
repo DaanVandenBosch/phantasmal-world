51ae313df214f99764097d7e84c57913293cf6cf
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2021-07-24 12:14:43 +0200

    Upgraded Gradle to 7.1.1 and Kotlin to 1.5.21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,22 @@ import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("js") version "1.4.32" apply false
-    kotlin("multiplatform") version "1.4.32" apply false
-    kotlin("plugin.serialization") version "1.4.32" apply false
+    kotlin("js") version "1.5.21" apply false
+    kotlin("multiplatform") version "1.5.21" apply false
+    kotlin("plugin.serialization") version "1.5.21" apply false
 }
 
 tasks.wrapper {
-    gradleVersion = "7.0"
+    gradleVersion = "7.1.1"
 }
 
 subprojects {
-    project.extra["coroutinesVersion"] = "1.4.3"
+    project.extra["coroutinesVersion"] = "1.5.1"
     project.extra["junitVersion"] = "5.7.1"
     project.extra["kotlinLoggingVersion"] = "2.0.6"
-    project.extra["ktorVersion"] = "1.5.3"
+    project.extra["ktorVersion"] = "1.6.1"
     project.extra["log4jVersion"] = "2.14.1"
-    project.extra["serializationVersion"] = "1.1.0"
+    project.extra["serializationVersion"] = "1.2.2"
     project.extra["slf4jVersion"] = "1.7.30"
 
     repositories {
