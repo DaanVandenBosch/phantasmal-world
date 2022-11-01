9cbc12cc242dfb56728d7b250cfa866114880092
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2022-11-01 22:47:51 +0100

    Fixed bug that would result in nested tabs "forgetting" which tab was active.

--- web/src/main/kotlin/world/phantasmal/web/core/controllers/PathAwareTabContainerController.kt
+++ web/src/main/kotlin/world/phantasmal/web/core/controllers/PathAwareTabContainerController.kt
@@ -1,7 +1,7 @@
 package world.phantasmal.web.core.controllers
 
 import world.phantasmal.cell.Cell
-import world.phantasmal.cell.map
+import world.phantasmal.cell.mutableCell
 import world.phantasmal.cell.mutateDeferred
 import world.phantasmal.web.core.PwToolType
 import world.phantasmal.web.core.stores.UiStore
@@ -17,17 +17,27 @@ open class PathAwareTabContainerController<T : PathAwareTab>(
     private val tool: PwToolType,
     final override val tabs: List<T>,
 ) : TabContainerController<T>() {
-    final override val activeTab: Cell<T?> =
-        map(uiStore.currentTool, uiStore.path) { currentTool, path ->
+    private val _activeTab = mutableCell(tabs.firstOrNull())
+    final override val activeTab: Cell<T?> = _activeTab
+
+    init {
+        observeNow(uiStore.currentTool, uiStore.path) { currentTool, path ->
             if (currentTool == tool) {
-                tabs.find { path.startsWith(it.path) } ?: tabs.firstOrNull()
-            } else {
-                null
+                val aTab = _activeTab.value
+
+                if (aTab == null || !path.startsWith(aTab.path)) {
+                    val newActiveTab = tabs.find { path.startsWith(it.path) }
+
+                    if (newActiveTab != null) {
+                        mutateDeferred {
+                            _activeTab.value = newActiveTab
+                        }
+                    }
+                }
             }
         }
 
-    init {
-        setPathPrefix(activeTab.value, replace = true)
+        setPathPrefix(_activeTab.value, replace = true)
     }
 
     final override fun setActiveTab(tab: T?) {
@@ -39,9 +49,7 @@ open class PathAwareTabContainerController<T : PathAwareTab>(
 
         if (visible) {
             mutateDeferred {
-                if (!disposed) {
-                    setPathPrefix(activeTab.value, replace = true)
-                }
+                setPathPrefix(_activeTab.value, replace = true)
             }
         }
     }
